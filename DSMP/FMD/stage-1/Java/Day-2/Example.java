3dec08aa42d51d762893886eb3e731bf19a04a9c
pavithri-apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-11-21 19:24:00 +0530

    Content update

---
+++ DSMP/FMD/stage-1/Java/Day-2/Example.java
@@ -0,0 +1,14 @@
+class Example{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.println(1234);
+        System.out.println(13.5);
+        System.out.println("Saman");
+        System.out.println(true);
+        System.out.println('A');
+        // This is a comment
+        /*This is multi 
+        line comment*/
+        
+    }
+}
