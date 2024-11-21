ab2dc22ac90baff671c851313badde6d56a4dedf
pavithri-apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-11-21 20:39:59 +0530

    Content update

--- DSMP/FMD/stage-1/Java/Day-2/Example.java
+++ DSMP/FMD/stage-1/Java/Day-2/Example.java
@@ -1,3 +1,5 @@
+
+
 class Example{
     public static void main(String args[]){
         System.out.println("Hello World");
@@ -9,6 +11,19 @@ class Example{
         // This is a comment
         /*This is multi 
         line comment*/
-        
+
+        //print vs println
+        System.out.print("Hello");
+        System.out.println("World");
+        System.out.println("b");
+
+        //variables
+        int data = 15;
+        System.out.println(data);
+
+        //string concatination vs arithmatic Addition
+        System.out.println("AB"+"CD");
+        System.out.println(10+"AB");
+
     }
 }
