15112ef896c026648d103fbdb50ebe1dff0d65f8
pavithri-apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-12-06 22:56:07 +0530

    Not null

--- DSMP/FMD/stage-1/MySQL/day-2.sql
+++ DSMP/FMD/stage-1/MySQL/day-2.sql
@@ -13,3 +13,12 @@ insert into t1 values ('C001','Kamal');
 #insert into t1 values ('C001','Jagath');
 insert into t1 values ('C002','Jagath');
 select * from t1;
+
+#Not null
+create table t2(
+    id int Primary Key,
+    name varchar(45) NOT NULL
+);
+desc t2;
+insert into t2 values (1001,'Kamal');
+insert into t2 values (1002,'Nimal');
