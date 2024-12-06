0553e6293af29ed37771e18be552a87467649720
pavithri-apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-12-06 23:01:48 +0530

    Unique

--- DSMP/FMD/stage-1/MySQL/day-2.sql
+++ DSMP/FMD/stage-1/MySQL/day-2.sql
@@ -22,3 +22,13 @@ create table t2(
 desc t2;
 insert into t2 values (1001,'Kamal');
 insert into t2 values (1002,'Nimal');
+
+#UNIQUE
+create table t3(
+    id int Primary key,
+    name varchar(45) not null,
+    nic varchar(45) UNIQUE,
+    email varchar(45) UNIQUE
+)
+insert into t3 values (1001,'Jagath',2001,'jagath@gmail.com');
+insert into t3 values (1002,'Saman',2005,'saman@gmail.com');
